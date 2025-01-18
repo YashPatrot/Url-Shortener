e752c8f06bc0820496dc3422a4ed5bfa6295adb6
Yash Patrot <yashyamanappapatrot@gmail.com>
2025-01-18 09:02:28 +0000

    feat:feat/prisma update Prisma configuration to use PostgreSQL and add extensions

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,18 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-    async onModuleInit() {
-      await this.$connect();
-    }
+
+  constructor() {
+    // Apply extensions during initialization
+    super()
+    this.$extends(withAccelerate());
   }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
